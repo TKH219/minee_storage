9687c2e6174191c59d9b5d4b3081f162900561af
James Tran <tkh219@gmail.com>
2026-08-21 19:36:24 +0700

    feat(onboarding): add the create shop screen

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -8,6 +8,7 @@ import 'package:mine_storage/features/auth/sign_in/pages/sign_in_page.dart';
 import 'package:mine_storage/features/auth/sign_up/pages/sign_up_page.dart';
 import 'package:mine_storage/features/dashboard/pages/dashboard_page.dart';
 import 'package:mine_storage/features/home/pages/home_page.dart';
+import 'package:mine_storage/features/onboarding/create_store/pages/create_store_page.dart';
 import 'package:mine_storage/features/onboarding/profile/pages/profile_page.dart';
 import 'package:mine_storage/features/products/pages/product_list_page.dart';
 import 'package:mine_storage/features/reports/pages/reports_page.dart';
@@ -63,7 +64,7 @@ final routerProvider = Provider<GoRouter>((ref) {
       GoRoute(
         path: AppRoutes.createStore,
         name: AppRoutes.createStoreName,
-        pageBuilder: (context, state) => _fadePage(state, const SizedBox.shrink()),
+        pageBuilder: (context, state) => _fadePage(state, const CreateStorePage()),
       ),
       GoRoute(
         path: AppRoutes.settings,
